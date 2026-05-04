7d1553e3f2a4d4b72c9c558543eb113fc1c7fde2
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-04 16:25:01 -0300

    feat: componente header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.module.css'
+import Header from './components/header/Header.jsx'
+
 
 function App() {
 
   return (
-    <></>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css'
+
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <a href="./"
+            >&lt;Projeto&gt;</a>
+        </header>
+    )
+}

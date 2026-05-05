6f5d12d1ec0c772e8bed4ed21465b65b85a59b91
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-05 12:40:19 -0300

    feat: customização icone elixir

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from './Card.module.css'
 
-export default function Card(){
-    return(
+export default function Card(props) {
+    return (
         <div className={styles.cardBox}>
-            <img src="card.webp" alt="" />
-            <p>Nome</p>
+            <img src={props.img} alt="" />
+            <p>{props.name}</p>
             <ul className={styles.row}>
-                <img src="elixir.jpg" alt="" />
-                <span>Raridade</span>       
+                <div className={styles.elixir}>
+                    <img src="elixir.png" alt="" />
+                    <div>{props.elixirCount}</div>
+                </div>
+                <span className={styles.rarity}>{props.rarity}</span>
             </ul>
             <button className={styles.btn}>Veja mais</button>
         </div>

0f684c58a121026228640f1bb799926355649e2a
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-07 13:27:53 -0300

    feat: importação do componente header na nova pagina; feat: position fixed retirado do header

--- src/pages/card-details/CardDetails.jsx
+++ src/pages/card-details/CardDetails.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import styles from './CardDetails.module.css'
+import Header from '../../components/header/Header'
 
 export default function CardDetails(){
     return(
-        <></>
+        <div>
+            <Header/>
+            <h1>hello world</h1>
+        </div>
     )
 }

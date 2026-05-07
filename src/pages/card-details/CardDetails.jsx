f9f181147cb1c34d48491b4380095534ecd1f602
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-07 15:31:43 -0300

    feat: estrutura hmtl estatica, css basico

--- src/pages/card-details/CardDetails.jsx
+++ src/pages/card-details/CardDetails.jsx
@@ -3,15 +3,35 @@ import { useNavigate } from 'react-router-dom'
 import styles from './CardDetails.module.css'
 import Header from '../../components/header/Header'
 
-export default function CardDetails(){
+export default function CardDetails() {
     const navigate = useNavigate()
     const handleClick = () => {
         navigate('/')
     }
-    return(
+    return (
         <div>
-            <Header/>
-            <button onClick={handleClick}>Voltar</button>
+            <Header />
+            <div className={styles.content}>
+                <div className={styles.col1}>
+                    <button onClick={handleClick}>Voltar</button>
+                    <img src="" alt="" />
+                    <div className={styles.sku}>
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div className={styles.col2}>
+                    <h1>Nome</h1>
+                    <div>
+                        <img src="elixir.png" alt="" />
+                        <div>0</div>
+                    </div>
+                    <div className={styles.maxLevels}>
+                        <span>MaxLevel</span>
+                        <span>MaxEvolutionLevel</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

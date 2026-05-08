252487fa7ca7bbe1ce369151d3a22b629930a432
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-08 13:03:12 -0300

    feat: import do useSearchParams para pegar o id do agente clicado e exibir na pagina de detalhes

--- src/pages/card-details/CardDetails.jsx
+++ src/pages/card-details/CardDetails.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './CardDetails.module.css'
 import Header from '../../components/header/Header'
 
 export default function CardDetails() {
     const navigate = useNavigate()
+    const [searchParams] = useSearchParams()
+    const id = searchParams.get('id')
     const handleClick = () => {
         navigate('/')
     }
@@ -15,21 +17,9 @@ export default function CardDetails() {
                 <div className={styles.col1}>
                     <button onClick={handleClick}>Voltar</button>
                     <img src="" alt="" />
-                    <div className={styles.sku}>
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                    </div>
                 </div>
                 <div className={styles.col2}>
-                    <h1>Nome</h1>
-                    <div>
-                        <img src="elixir.png" alt="" />
-                        <div>0</div>
-                    </div>
-                    <div className={styles.maxLevels}>
-                        <span>MaxLevel</span>
-                        <span>MaxEvolutionLevel</span>
-                    </div>
+                    <h1>{id}</h1>
                 </div>
             </div>
         </div>

0a835ece182fa3f37c84d4181bca3fdff52ce896
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-08 11:09:38 -0300

    fix: mudança estrutura e props do componente para a nova API

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Card.module.css'
 
-export default function Card({key, id, img, name, elixirCount, rarity }) {
+export default function Card({ key, id, img, name, roleIcon, role}) {
     const navigate = useNavigate()
     const handleClick = () => {
         navigate(`/details?id=${id}`)
@@ -10,14 +10,13 @@ export default function Card({key, id, img, name, elixirCount, rarity }) {
     return (
         <div className={styles.cardBox} key={key}>
             <img src={img} alt="" />
-            <p className={styles.name}>{name}</p>
-            <ul className={styles.row}>
-                <div className={styles.elixir}>
-                    <img src="elixir.png" alt="" />
-                    <div>{elixirCount}</div>
-                </div>
-                <span className={styles.rarity}>{rarity}</span>
-            </ul>
+            <div>
+                <p className={styles.name}>{name}</p>
+                <ul className={styles.role}>
+                    <img src={roleIcon} alt="" />
+                    <span>{role}</span>
+                </ul>
+            </div>
             <button className={styles.btn} onClick={handleClick}>Veja mais</button>
         </div>
     )

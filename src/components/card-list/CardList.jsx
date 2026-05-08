efc7b36883bced1ad219c6ca9e77a6a371ea293c
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-08 11:20:17 -0300

    fix: passando os atributos certos para o component, fetch da API do Valorant

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import styles from './CardList.module.css'
 import Card from '../card/Card.jsx'
 
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjVhYWNhZWZhLTJjMzEtNGI0OC1hZTc3LThmZWY1NTNlODZmMiIsImlhdCI6MTc3Nzk5NjM3OSwic3ViIjoiZGV2ZWxvcGVyLzk3YjYwOTIwLTM4MzAtN2IwYi02M2QxLTA4MjQ5NTM0YzRhZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDEuNjMuMTM0LjQyIiwiMTc3LjEzOS4yNDUuMTE2Il0sInR5cGUiOiJjbGllbnQifV19.3Ppf314m57pbNSXISQGVVPqBgjKOFF2DQD2nJxmlXCeNlrtpmGbcKMJwvxEmSTQyiLfb_9s6kB9S4mhYlRbeCg";
+//const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjVhYWNhZWZhLTJjMzEtNGI0OC1hZTc3LThmZWY1NTNlODZmMiIsImlhdCI6MTc3Nzk5NjM3OSwic3ViIjoiZGV2ZWxvcGVyLzk3YjYwOTIwLTM4MzAtN2IwYi02M2QxLTA4MjQ5NTM0YzRhZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDEuNjMuMTM0LjQyIiwiMTc3LjEzOS4yNDUuMTE2Il0sInR5cGUiOiJjbGllbnQifV19.3Ppf314m57pbNSXISQGVVPqBgjKOFF2DQD2nJxmlXCeNlrtpmGbcKMJwvxEmSTQyiLfb_9s6kB9S4mhYlRbeCg";
 
 
 export default function CardList() {
@@ -11,10 +11,10 @@ export default function CardList() {
 
     useEffect(() => {
         try {
-            const responseFetch = fetch('/api/cards/', {
+            const responseFetch = fetch('https://valorant-api.com/v1/agents', {
                 method: 'GET',
                 headers: {
-                    'Authorization': `Bearer ${token}`,
+                    //'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json'
                 }
             })
@@ -34,21 +34,21 @@ export default function CardList() {
     if (loading) {
         return (<p><strong>Carregando...</strong></p>)
     }
-
-    if (content.length < 0 || !content?.items || content.items.lenght == 0) {
-        return (<p><strong>Cartas não encontradas</strong></p>)
+    console.log(content.data)
+    if (content.length < 0 || !content?.data || content.data.lenght == 0) {
+        return (<p><strong>Agentes não encontrados</strong></p>)
     }
 
     return (
         <div className={styles.grid}>
-            {content.items.map((item, index) =>
+            {content.data.map((item, index) =>
                 <Card
                     key={index}
-                    id={item.id}
-                    img={item.iconUrls?.medium}
-                    name={item.name}
-                    elixirCount={item.elixirCost}
-                    rarity={item.rarity}
+                    id={item.uuid}
+                    img={item.fullPortrait}
+                    name={item.displayName}
+                    role={item.role?.displayName}
+                    roleIcon={item.role?.displayIcon}
                 />
             )}
         </div>

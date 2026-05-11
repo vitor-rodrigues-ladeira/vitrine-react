724dfa605988585db3b118b59b16085bf54ddd66
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-11 14:26:36 -0300

    feat: estrura html da sidebar, regras basica de css para sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,37 @@
-import React from  'react'
+import React from 'react'
 import styles from './Sidebar.module.css'
 
-export default function Sidebar(){
-    return(
-        <div></div>
+export default function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <h3>Filtro</h3>
+            <form className={styles.form}>
+                <hr />
+                <div className={styles.roles}>
+                    <span>Função</span>
+                    <div className={styles.row}>
+                        <input type="checkbox" name='role' value='iniciador' />
+                        <label for='iniciador'>Iniciador</label>
+                    </div>
+                    <div className={styles.row}>
+                        <input type="checkbox" name='role' value='duelista' />
+                        <label for='duelista'>Duelista</label>
+                    </div>
+                    <div className={styles.row}>
+                        <input type="checkbox" name='role' value='sentinela' />
+                        <label for='sentinela'>Sentinela</label>
+                    </div>
+                    <div className={styles.row}>
+                        <input type="checkbox" name='role' value='controlador' />
+                        <label for='controlador'>Controlador</label>
+                    </div>
+                </div>
+                <hr />
+                <div className={styles.row}>
+                    <input type="checkbox" name='test' value='test' />
+                    <label for='test'>Disponível para teste</label>
+                </div>
+            </form>
+        </aside>
     )
 }
--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import styles from './App.module.css'
 import Header from '../../components/header/Header.jsx'
 import Footer from '../../components/footer/Footer.jsx'
+import Sidebar from '../../components/sidebar/Sidebar.jsx'
 import CardList from '../../components/card-list/CardList.jsx'
 
-export default function  App() {
+export default function App() {
   return (
     <div>
       <Header />
-      <CardList />
-      <Footer/>
+      <div className={styles.content}>
+        <Sidebar />
+        <CardList />
+      </div>
+      <Footer />
     </div>
   )
 }

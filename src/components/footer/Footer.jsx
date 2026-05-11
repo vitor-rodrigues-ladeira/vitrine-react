3698d5dbd499de195abd956d3e5f024f354bdb26
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-11 12:10:48 -0300

    fix: github abrindo em uma nova aba

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 export default function Footer(){
     return(
         <footer className={styles.footer}>
-            <h2 className={styles.text}>Vitor Paes Rodrigues - <a href="https://github.com/vitor-rodrigues-ladeira/">@vitor-rodrigues-ladeira</a></h2>
+            <h2 className={styles.text}>Vitor Paes Rodrigues - <a href="https://github.com/vitor-rodrigues-ladeira/" target='__blanck'>@vitor-rodrigues-ladeira</a></h2>
         </footer>
     )
 }
